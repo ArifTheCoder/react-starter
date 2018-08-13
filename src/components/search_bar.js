d4ee608ee583160fdb979651a1d580d285192bad
Arif Hossain <arifthecoder@gmail.com>
2018-08-13 21:38:51 +0300

    child components were created

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -2,10 +2,16 @@ import React, {Component} from 'react';
 
 
 class SearchBar extends Component {
+    constructor(props){
+        super(props);
+        this.state = {term: ''};
+    }
     render(){
         return(
-            <div>
-                <input onChange= {(event) => console.log(event.target.value)} />
+            <div className="search-bar">
+                <input 
+                value={this.state.term}
+                onChange= {(event) => this.setState({term: event.target.value})} />
             </div>
         );
     }

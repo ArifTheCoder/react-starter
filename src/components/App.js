8b844c2ab61ebf970f205f2f3899508f51fc9f8b
Arif Hossain <arifthecoder@gmail.com>
2018-08-13 21:37:41 +0300

    App componemt was created

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,32 @@
 import React, { Component } from 'react';
 import './App.css';
-
 import SearchBar from './search_bar';
-
+import VideoList from './video_list';
+import VideoDetail from './video_detail';
+import YTSearch from 'youtube-api-search';
 const API_KEY = 'AIzaSyBCVJpn3gnfSTyKieqo05WumMlFzWWDqiI';
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {videos: [], 
+      selectedVideo: null
+    };
+
+    YTSearch({key: API_KEY, term: 'tom-cat'}, (videos) => {
+      this.setState({videos: videos,
+        selectedVideo: videos[0]
+      });        // this.setState({videos: videos})
+    });
+  }
   render() {
     return (
       <div className="App">
        <SearchBar />
+       <VideoDetail  video={this.state.selectedVideo}/>
+       <VideoList 
+       onVideoSelect={(selectedVideo) => this.setState({selectedVideo})}
+       videos={this.state.videos}/>
       </div>
     );
   }
